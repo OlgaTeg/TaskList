20369e1230d4c834698fffd75c546cc7d59f7df8
OlgaTeg <tegza.o@yandex.ru>
2022-12-27 17:45:17 +0300

    add delete function add StorageManager

--- TaskList/ViewControllers/TaskListViewController.swift
+++ TaskList/ViewControllers/TaskListViewController.swift
@@ -1,30 +1,25 @@
-//
-//  ViewController.swift
-//  TaskList
-//
-//  Created by Vasichko Anna on 26.12.2022.
-//
 
 import UIKit
 import CoreData
-
-protocol TaskViewControllerDelegate {
-    func reloadData()
-}
 
 class TaskListViewController: UITableViewController {
     
     private let cellID = "task"
     private var taskList: [Task] = []
-    private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
+    private let viewContext = StorageManager.shared.persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupNavigationBar()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        fetchData()
     }
+}
 
+//MARK: - Private methods
+
+extension TaskListViewController {
     private func setupNavigationBar() {
         title = "Task List"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -45,13 +40,10 @@ class TaskListViewController: UITableViewController {
         )
         
         navigationController?.navigationBar.tintColor = .white
-        
     }
     
     @objc private func addNewTask() {
-        let taskVC = TaskViewController()
-        taskVC.delegate = self
-        present(taskVC, animated: true)
+        showAlert(withTitle: "New Task", andMessage: "Please add new task!")
     }
     
     private func fetchData() {
@@ -61,6 +53,59 @@ class TaskListViewController: UITableViewController {
             taskList = try viewContext.fetch(fetchRequest)
         } catch let error {
             print("No fetch data", error)
+        }
+    }
+    
+    private func showAlert(
+        withTitle title: String,
+        andMessage message: String
+    ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        
+        let task = Task(context: viewContext)
+       
+        task.title = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch let error {
+                print(error)
+            }
+        }
+    }
+
+    private func deleteTask(_ task: Task, indexPath: IndexPath) {
+        viewContext.delete(task)
+        
+        do {
+            try viewContext.save()
+            taskList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        } catch let error {
+            print(error)
         }
     }
 }
@@ -80,12 +125,12 @@ extension TaskListViewController {
         cell.contentConfiguration = content
         return cell
     }
-}
-
-// MARK: - TaskViewControllerDelegate
-extension TaskListViewController: TaskViewControllerDelegate {
-    func reloadData() {
-        fetchData()
-        tableView.reloadData()
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let task = taskList[indexPath.row]
+        
+        if editingStyle == .delete {
+            deleteTask(task, indexPath: indexPath)
+        }
     }
 }
